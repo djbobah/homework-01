66b4300518ef7ffc492dce60b3ab3385b335694b
Vladimir Kirpichev <djbobah@mail.ru>
2021-09-02 16:35:26 +0300

    module 2 task3.1 done

--- counter/src/component/counter.jsx
+++ counter/src/component/counter.jsx
@@ -1,46 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Counter = (props) => {
-  console.log(props);
-  const [value, setValue] = useState(props.value);
-
-  const [imageUrl, setImageUrl] = useState("https://picsum.photos/200");
-
+const Counter = ({ id, value, name, onIncrement, onDecrement, onDelete }) => {
   const formValue = () => {
     return value === 0 ? "Ноль" : value;
   };
 
-  const getBageClasses = () => {
+  const getBadgeClasses = () => {
     let classes = "badge m-2 bg-";
     classes += value === 0 ? "danger" : "primary";
     return classes;
   };
 
-  const handleIncrement = (productId) => {
-    setValue(value + 1);
-  };
-
-  const handleDecrement = (productId) => {
-    if (value > 0) {
-      setValue(value - 1);
-    }
-  };
-
   return (
     <div>
-      <h4>{props.name}</h4>
-      <span className={getBageClasses()}>{formValue()}</span>
+      <h4>{name}</h4>
+      <span className={getBadgeClasses()}>{formValue()}</span>
       <button
-        onClick={() => handleIncrement({ id: 1 })}
+        onClick={() => onIncrement(id)}
         className="btn btn-secondary btn-sm"
       >
         Increment
       </button>
       <button
-        onClick={() => handleDecrement()}
+        onClick={() => onDecrement(id)}
         className="btn btn-secondary btn-sm"
       >
         Decrement
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => onDelete(id)}
+      >
+        delete
       </button>
     </div>
   );
--- counter/src/component/counters.jsx
+++ counter/src/component/counters.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
 import Counter from "./counter";
 
-const Counters = () => {
-  const [counters, setCounters] = useState([
-    { value: 0, id: 1, name: "Ложка" },
-    { value: 4, id: 2, name: "Вилка" },
-    { value: 0, id: 3, name: "Тарелка" },
-    { value: 0, id: 3, name: "Стартовый набор минималиста" },
-    { value: 0, id: 3, name: "Ненужные вещи" },
-  ]);
+const Counters = ({ onReset, counters, ...rest }) => {
   return (
     <div>
+      <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+        Сброс
+      </button>
       {counters.map((counter) => (
-        <Counter key={Counter.id} value={counter.value} name={counter.name} />
+        <Counter key={Counter.id} {...counter} {...rest} />
       ))}
     </div>
   );
